88ca3b11d96406b06cf2f6f74ee2ce8b7f8ed6c1
Larissa4-droid <l.iriza@alustudent.com>
2024-11-09 23:48:19 +0200

    Update 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""Import Square class."""
+"""
+This module contains a class
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Square class inherits from Rectangle."""
-
+    """
+    class Square that inherits from rectangle
+    """
     def __init__(self, size):
-        """Initialize square with size."""
+        """
+        class instantiation
+        """
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
-
-    def __str__(self):
-        """Return string of square."""
-        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
+        super().__init__(size, size)
