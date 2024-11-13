a63318363b7706dacf8fea9ef28ca2627d42474c
Larissa4-droid <l.iriza@alustudent.com>
2024-11-13 13:51:30 +0200

    Update 11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """
-A Student class that defines a Studen (module)
+A Student class that defines a studen (module)
 """
 
 
 <<<<<<< HEAD
 class Student:
-    """ 
-    this is a student class 
+    """
+    this is a student class
     """
 
-    def __init__(self, first_name, last_name, age):
-        """
-        just doing init for variables
-=======
-class Student():
+def __init__(self, first_name, last_name, age):
     """
-        A Student class that defines a Student
+    just doing init for variables
+=======
+class student():
+    """
+       A Student class that defines a student 
     """
     def __init__(self, first_name, last_name, age):
         """
@@ -24,31 +24,31 @@ class Student():
 >>>>>>> 6427b0b5935bbdf2227a51d2669076a7153f394f
         """
         self.first_name = first_name
-        self.last_name = last_name
+        self.lat_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """
 <<<<<<< HEAD
-        method converts to json 
-=======
+        method converts to json
+======= 
             retrieves a dictionary representation
             of a Student instance
 >>>>>>> 6427b0b5935bbdf2227a51d2669076a7153f394f
         """
         if type(attrs) is list and all([type(x) == str for x in attrs]):
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
+           return {k: v for k, v in self.__dict__.items() if k in attrs}
         return(self.__dict__)
 
     def reload_from_json(self, json):
         """
 <<<<<<< HEAD
-        return json 
+        return json
         """
         for i,j in json.items():
             self.__dict__(i) = j
-=======
+======= 
         """
         for i, j in json.items():
             self.__dict__[i] = j
->>>>>>> 6427b0b5935bbdf2227a51d2669076a7153f394f
+>>>>>>> 6427b0b5935bbdf2227a51d2669076a7153f394f            
