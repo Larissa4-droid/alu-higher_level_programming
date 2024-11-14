d7be5cff4c740e4d67cf06da34d07c1e536d8712
Larissa4-droid <l.iriza@alustudent.com>
2024-11-14 14:25:49 +0200

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""
-Module that adds command-line arguments to a JSON file list.
-"""
+"""adds all arguments to a Python list, and after that save them to a file:"""
+
+
 import sys
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file') \
+        .load_from_json_file
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    try:
+        loadFile = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        loadFile = []
 
-filename = "add_item.json"
-
-# Load the existing list from the file if it exists;
-try:
-    items = load_from_json_file(filename)
-except FileNotFoundError:
-    items = []
-
-items.extend(sys.argv[1:])
-
-save_to_json_file(items, filename)
+    argc = len(sys.argv)
+    for idx in range(1, argc):
+        loadFile.append(sys.argv[idx])
+    save_to_json_file(loadFile, "add_item.json")
