301ed8fea3436a0a1a0569c3bc8df5d91f419693
Larissa4-droid <l.iriza@alustudent.com>
2024-11-15 14:19:47 +0200

    Update 11-genre_id_all_shows.sql

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,15 +1,5 @@
--- Prints records with column values in a set formed
--- from the intersection of multiple tables
-SELECT DISTINCT name
-    FROM tv_genres
-    WHERE name NOT IN
-    (
-        SELECT a.name
-            FROM tv_genres a
-            INNER JOIN tv_show_genres b
-                ON a.id = b.genre_id
-            INNER JOIN tv_shows c
-                ON c.id = b.show_id
-            WHERE c.title = 'Dexter'
-    )
-    ORDER BY name ASC;
+-- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
