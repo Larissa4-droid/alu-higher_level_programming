42f9b40a9dff880c3e243dfbbb2f6b6b1828f8b1
Larissa4-droid <l.iriza@alustudent.com>
2024-12-01 08:43:41 +0200

    Update model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,6 +3,7 @@
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
+
 class City(Base):
     """Class representing cities table"""
     __tablename__ = 'cities'
